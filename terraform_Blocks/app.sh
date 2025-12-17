9d708ae09b019287b19d5fd945bcc790a93de02f
Nagaraju Kuruva <nagaraju.kuruva@in.techwave.net>
2025-12-17 14:32:06 +0530

    added sh file

--- terraform_Blocks/app.sh
+++ terraform_Blocks/app.sh
@@ -1,5 +1,6 @@
 sudo apt update -y
-sudo apt install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo apt install apache2 -y
+sudo systemctl status apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
 echo "<h1>welcome to ASG $(hostname -f)</h1>" > /var/www/html/index.html
